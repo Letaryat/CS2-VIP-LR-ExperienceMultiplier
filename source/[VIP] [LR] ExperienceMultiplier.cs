dec1fd34a16010e9d6fe508b4e77c06c8e2006a2
Letaryat <69041891+Letaryat@users.noreply.github.com>
2024-07-15 10:10:36 +0200

    s

--- source/[VIP] [LR] ExperienceMultiplier.cs
+++ source/[VIP] [LR] ExperienceMultiplier.cs
@@ -1,59 +1,53 @@
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Capabilities;
-using VipCoreApi;
+using CounterStrikeSharp.API.Modules.Admin;
 using LevelsRanksApi;
 namespace VIP_ExperienceMultiplier;
 
 public class VipExperienceMultiplier : BasePlugin
 {
-    public override string ModuleAuthor => "ABKAM";
-    public override string ModuleName => "[VIP] Experience Multiplier";
+    public override string ModuleAuthor => "Letaryat";
+    public override string ModuleDescription => "Modified plugin CS2-VIP-LR-ExperienceMultiplier to multiply experience for C# LR when player has specific flag";
+    public override string ModuleName => "[VIP-Plugin] Experience Multiplier";
     public override string ModuleVersion => "v1.0.0";
 
-    private IVipCoreApi? _api;
+
     private ExperienceMultiplier? _experienceMultiplier;
 
-    private PluginCapability<IVipCoreApi> PluginCapability { get; } = new("vipcore:core");
     private PluginCapability<ILevelsRanksApi> LevelsRanksCapability { get; } = new("levels_ranks");
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
-        _api = PluginCapability.Get();
         var levelsRanksApi = LevelsRanksCapability.Get();
 
-        if (_api == null || levelsRanksApi == null) return;
-
-        _experienceMultiplier = new ExperienceMultiplier(_api, levelsRanksApi);
-        _api.RegisterFeature(_experienceMultiplier);
     }
 
-    public override void Unload(bool hotReload)
-    {
-        _api?.UnRegisterFeature(_experienceMultiplier!);
-    }
 }
 
-public class ExperienceMultiplier : VipFeatureBase
+public class ExperienceMultiplier 
 {
     private readonly ILevelsRanksApi _levelsRanksApi;
 
-    public override string Feature => "ExperienceMultiplier";
 
-    public ExperienceMultiplier(IVipCoreApi api, ILevelsRanksApi levelsRanksApi) : base(api)
+    public ExperienceMultiplier(ILevelsRanksApi levelsRanksApi)
     {
         _levelsRanksApi = levelsRanksApi;
     }
 
-    public override void OnPlayerSpawn(CCSPlayerController player)
+    public void OnPlayerSpawn(CCSPlayerController player)
     {
+        if (AdminManager.PlayerInGroup(player, "@vip-plugin/vip"))
+        {
+        var steamId = player.SteamID.ToString();
+        _levelsRanksApi.SetExperienceMultiplier(steamId, 1.5);
+        }
+        /*
         if (!PlayerHasFeature(player)) return;
         if (GetPlayerFeatureState(player) is not IVipCoreApi.FeatureState.Enabled) return;
-
-        var steamId = player.SteamID.ToString();
-        var multiplier = GetFeatureValue<double>(player);
-
         if (multiplier <= 0) return;
-
+        *?
         _levelsRanksApi.SetExperienceMultiplier(steamId, multiplier);
+        */
     }
 }
